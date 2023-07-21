b3382ff4c3f5a22b947e113e0bf599042035182c
guguiness <gus.nogueira@proton.me>
2023-07-21 17:42:08 -0300

    Renomeando função 'criaMemoria' e declarando e inicializando espaços de memória em 'main'

--- main.c
+++ main.c
@@ -3,14 +3,22 @@
 
 // considerar para a memoria: 0 = lacuna / 1 = processo
 
-void criaMemoria(int *memoria) {         // memoria inicialmente "vazia"
+void inicializaMemoria(int *memoria) {         // memoria inicialmente "vazia"
     for(int i = 0; i < TAM; i++) {
         memoria[i] = 0;
     } 
 }
 
 int main() {
+    int memoria_ff[TAM];
+    int memoria_bf[TAM];
+    int memoria_wf[TAM];
+    int memoria_cf[TAM];
 
+    inicializaMemoria(memoria_ff);
+    inicializaMemoria(memoria_bf);
+    inicializaMemoria(memoria_wf);
+    inicializaMemoria(memoria_cf);
 
     printf("\n");
     return 0;
