b14b86c974cd9f93a76e41412fe414007ae281e5
guguiness <gus.nogueira@proton.me>
2023-07-21 17:25:56 -0300

    Adicionando função criaMemoria e definindo constante TAM

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#define TAM 10
+
+// considerar para a memoria: 0 = lacuna / 1 = processo
+
+void criaMemoria(int *memoria) {         // memoria inicialmente "vazia"
+    for(int i = 0; i < TAM; i++) {
+        memoria[i] = 0;
+    } 
+}
 
 int main() {
 
