cfbb71a3c90cd18225270840b2fd0c4a7ee69a3f
guguiness <gus.nogueira@proton.me>
2023-07-29 19:58:03 -0300

    Adicionando variável 'qntdProcessos' e função 'insereFirstFit'

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #define TAM 10
 
+int qntdProcessos = 0;          // PIDs existentes na memoria (compartilhado??)
+
 // considerar para a memoria: 0 = lacuna / 1 = processo
 
 void inicializaMemoria(int *memoria) {         // memoria inicialmente "vazia"
@@ -13,6 +15,37 @@ void imprimeVetor(int *vet) {
     printf("|");
     for(int i = 0; i < TAM; i++)
         printf(" %d |");
+}
+
+int insereFirstFit(int *vetor, int tamProcesso) {
+    int qntdBuracosConsecutivos = 0;
+    int anterior = -1;                  // valor igual a 0: elemento anterior era buraco
+    int posIncioBuraco;                 // posicao onde comeca uma sequencia de buracos
+
+    if(tamProcesso > TAM) return 1;
+
+    for(int i = 0; i < TAM || qntdBuracosConsecutivos != tamProcesso; i++) {
+        if(vetor[i] == 0) {
+            qntdBuracosConsecutivos++;
+            if(anterior != 0) posIncioBuraco = i;
+        } else {
+            if(anterior == 0) qntdBuracosConsecutivos = 0;
+        }
+    }
+
+    if(qntdBuracosConsecutivos == tamProcesso) {
+        // insere o processo no vetor
+        int cont = 0;
+        qntdProcessos++;            // PID do novo processo
+        for(int i = posIncioBuraco; cont != tamProcesso; i++) {
+            vetor[i] = qntdProcessos;
+            cont++;
+        }
+        return 0;
+    } else {
+        // sem espaco suficiente
+        return 1;
+    }
 }
 
 int main() {
